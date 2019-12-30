aaec1711d0b9f44d3111eb572c04fda1b10a689a
Tatsuya Kanemoto <cashewnuts903@gmail.com>
2019-12-30 18:31:18 +0900

    ADD create index query.

--- graphql/schema/schema.js
+++ graphql/schema/schema.js
@@ -1,20 +1,57 @@
 const graphql = require('graphql')
+const axios = require('axios')
+const querystring = require('querystring');
+const _ = require('lodash')
 
-const { GraphQLObjectType, GraphQLString, GraphQLSchema, GraphQLID } = graphql
+const { GraphQLObjectType, GraphQLString, GraphQLSchema, GraphQLID, GraphQLList } = graphql
 
-// dummy data
-const books = [
-  { name: 'Name of the Wind', genre: 'Fantsy', id: '1' },
-  { name: 'The Final Empire', genre: 'Fantsy', id: '2' },
-  { name: 'The Long Earth', genre: 'Sci-Fi', id: '3' },
-]
+const INDEX_API = 'https://dashboard.e-stat.go.jp/api/1.0/Json/getIndicatorInfo'
 
-const BookType = new GraphQLObjectType({
-  name: 'Book',
+const AnnotationType = new GraphQLObjectType({
+  name: 'annotation',
+  fields: {
+    cycle: { type: GraphQLString },
+    regionalRank: { type: GraphQLString },
+    isSeasonal: { type: GraphQLString },
+    annotation: { type: GraphQLString },
+  }
+})
+const DetailType = new GraphQLObjectType({
+  name: 'detail',
+  fields: {
+    code: { type: GraphQLString },
+    name: { type: GraphQLString },
+    description: { type: GraphQLString },
+  }
+})
+const ClassType = new GraphQLObjectType({
+  name: 'class',
+  fields: {
+    name: { type: GraphQLString },
+    sname: { type: GraphQLString },
+    statName: { type: GraphQLString },
+    unit: { type: GraphQLString },
+    indicatorClass: { type: GraphQLString },
+    cycle: { type: GraphQLString },
+    regionalRank: { type: GraphQLString },
+    isSeasonal: { type: GraphQLString },
+    from: { type: GraphQLString },
+    to: { type: GraphQLString },
+  }
+})
+
+const IndexType = new GraphQLObjectType({
+  name: 'Index',
   fields: () => ({
     id: { type: GraphQLID },
-    name: { type: GraphQLString },
-    genre: { type: GraphQLString },
+    indicatorCode: { type: GraphQLString },
+    indicatorName: { type: GraphQLString },
+    indicatorElementName: { type: GraphQLString },
+    statName: { type: GraphQLString },
+    statCode: { type: GraphQLString },
+    details: { type: new GraphQLList(DetailType) },
+    classes: { type: new GraphQLList(ClassType) },
+    annotations: { type: new GraphQLList(AnnotationType) }
   })
 })
 
@@ -22,14 +59,61 @@ const BookType = new GraphQLObjectType({
 const RootQuery = new GraphQLObjectType({
   name: 'RootQueryType',
   fields: {
-    book: {
-      type: BookType,
-      args: { id: { type: GraphQLID } },
-      resolve(parent, args) {
-        console.log(typeof args.id)
-        return books.find((b) => b.id === args.id)
+    index: {
+      type: new GraphQLList(IndexType),
+      args: { category: { type: GraphQLString }, statCode: { type: GraphQLString }},
+      async resolve(parent, args) {
+        const { data } = await axios.get(INDEX_API, querystring.stringify({ Category: args.category, StatCode: args.statCode}));
+        const results = _.get(data, 'GET_META_INDICATOR_INF.METADATA_INF.CLASS_INF.CLASS_OBJ', 'default');
+        const indexes = results.map((result) => {
+          let details = _.get(result, 'details.detail', null)
+          if (details) {
+            details = details.map((d) => ({
+              code: d['@code'],
+              name: d['@name'],
+              description: d['$'],
+            }))
+          }
+          let annotations = _.get(result, 'annotations', null)
+          if (annotations) {
+            annotations = annotations.map((ann) => ({
+              cycle: ann['@cycle'],
+              regionalRank: ann['@regionalRank'],
+              isSeasonal: ann['@isSeasonal'],
+              annotation: ann['@annotation']
+            }))
+          }
+          const classes = result.CLASS.map((cls) => {
+            const cycle = _.get(cls, 'cycle.@code')
+            const regionalRank = _.get(cls, 'RegionalRank.@code')
+            const isSeasonal = _.get(cls, 'IsSeasonal.@code')
+            return {
+              name: cls['@name'],
+              sname: cls['@sname'],
+              from: cls['@fromDate'],
+              to: cls['@toDate'],
+              statName: cls['@statName'],
+              unit: cls['@unit'],
+              cycle,
+              regionalRank,
+              isSeasonal,
+              indicatorClass: '0' + cycle + '0' + regionalRank + '0' + isSeasonal,
+            }
+          })
+          return {
+            id: '???',
+            indicatorCode: result['@code'],
+            indicatorName: result['@name'],
+            statCode: result['@name'],
+            statName: result['@name'],
+            details,
+            annotations,
+            classes,
+          }
+        })
+        return indexes;
       },
-    },
+    }
   }
 })
 
